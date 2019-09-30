0952019260e92337dc42cc234b4da8694b7985a3
MuskChan <523919565@qq.com>
2019-09-30 20:36:54 +0800

    Use Redis and task scheduling

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Laravel\Passport\HasApiTokens;
 class User extends Authenticatable
 {
     use Notifiable, HasApiTokens;
+    use Traits\LastActivedAtHelper;
 
     /**
      * The attributes that are mass assignable.
